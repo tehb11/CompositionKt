0becc98247127177395d5838bcdc93283f9fb466
Ivan Sinyakin <i.sinyakin@brlab.net>
2024-10-20 18:00:34 +1000

    07.6 add ViewBinding. Change Fragments

--- app/src/main/java/ru/illidan/composition/presentation/WelcomeFragment.kt
+++ app/src/main/java/ru/illidan/composition/presentation/WelcomeFragment.kt
@@ -6,13 +6,31 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import ru.illidan.composition.R
+import ru.illidan.composition.databinding.FragmentWelcomeBinding
 
 class WelcomeFragment : Fragment() {
+
+    private var _binding: FragmentWelcomeBinding? = null // служебный с варинатом null
+    private val binding: FragmentWelcomeBinding // не null экземпляр FragmentWelcomeBinding, с геттером ниже (чтобы не обрабатывать состояние что возможно null)
+        get() = _binding ?: throw RuntimeException("FragmentWelcomeBinding == null")
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_welcome, container, false)
+        _binding = FragmentWelcomeBinding.inflate(inflater, container, false ) // создаем экземпляр FragmentWelcomeBinding
+        return binding.root // возвращаем View
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.buttonUnderstand.setOnClickListener{
+
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
